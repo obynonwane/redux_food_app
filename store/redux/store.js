33ac8030e3b671cafe059430680bcadf4d32ced0
obynonwane <biostechnologyng@gmail.com>
2024-08-17 16:52:26 +0100

    Working with Redux Slices

--- store/redux/store.js
+++ store/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import favoritesReducer from "./favorites";
 
 // 2. create store
 export const store = configureStore({
-    reducer:{}
-})
+  reducer: {
+    favoriteMeals: favoritesReducer,
+  },
+});
